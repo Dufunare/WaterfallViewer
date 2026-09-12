31b33895e5999793cde45ec30ca33f89b80bf3d7
Chenyu Du <funare2022@gmail.com>
2026-09-12 20:00:19 +0800

    feat(resource): register media URI protocol

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,26 @@
 mod ipc;
+mod media_resource;
 
 use ipc::{cancel_scan, start_scan, ScanRegistry};
+use media_resource::{respond_to_media_request, MediaResourceRegistry, MEDIA_PROTOCOL};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let resources = MediaResourceRegistry::default();
+    let protocol_resources = resources.clone();
+
     tauri::Builder::default()
         .manage(ScanRegistry::default())
+        .manage(resources)
+        .register_asynchronous_uri_scheme_protocol(
+            MEDIA_PROTOCOL,
+            move |_context, request, responder| {
+                let registry = protocol_resources.clone();
+                std::thread::spawn(move || {
+                    responder.respond(respond_to_media_request(registry, request));
+                });
+            },
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![start_scan, cancel_scan])
         .run(tauri::generate_context!())
