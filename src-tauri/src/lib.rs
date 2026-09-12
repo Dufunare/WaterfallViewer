1b6fb26baf55359f5b4839d0c1d2d2b93aa78247
Duchenyu <funare2022@gmail.com>
2026-09-12 17:32:19 +0800

    chore:reboot

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,14 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn hello_from_rust() -> String {
-    "Hello from Rust! 前后端链路已经打通。".to_string()
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![hello_from_rust])
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
