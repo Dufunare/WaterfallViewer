c5c3735bdbaacc4690872c4d490fffc1862178ae
Chenyu Du <funare2022@gmail.com>
2026-09-13 13:17:52 +0800

    refactor(selection): provide shared viewer selection

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { provide } from "vue";
+
 import type { MediaBrowserController } from "./application/browser/mediaBrowserController";
 import type { CanvasBrowserController } from "./application/canvas/canvasBrowserController";
 import type { MediaQueryController } from "./application/query/mediaQueryController";
@@ -8,6 +10,7 @@ import type { PreviewMediaDetailController } from "./application/viewer/previewM
 import type { ViewerWorkspaceController } from "./application/viewer/viewerWorkspaceController";
 import MediaPreviewOverlay from "./presentation/components/MediaPreviewOverlay.vue";
 import ViewerPage from "./presentation/pages/ViewerPage.vue";
+import { mediaSelectionKey } from "./presentation/selectionContext";
 
 const props = defineProps<{
   workspace: ViewerWorkspaceController;
@@ -19,6 +22,8 @@ const props = defineProps<{
   previewDetails: PreviewMediaDetailController;
 }>();
 
+provide(mediaSelectionKey, props.selection);
+
 function activateMedia(mediaId: string): void {
   props.activation.activate(mediaId);
 }
@@ -28,7 +33,6 @@ function activateMedia(mediaId: string): void {
   <ViewerPage
     :workspace="workspace"
     :query="query"
-    :selection="selection"
     :flow-browser="flowBrowser"
     :create-canvas-browser="createCanvasBrowser"
     @activate="activateMedia"
